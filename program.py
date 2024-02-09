72c0096ea7b96e513115902108663c052fdf443c
dorpivovar <armandooganesyan@gmail.com>
2024-02-09 11:59:52 +0300

    first commit

--- program.py
+++ program.py
@@ -179,5 +179,36 @@ def astronaut_selection():
         return "Форма отправлена"
 
 
+@app.route('/choice/<planet_name>')
+def choice(planet_name):
+    return f"""<!doctype html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                        <link rel="stylesheet" 
+                        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                        crossorigin="anonymous">
+                        <title>Привет, Марс!</title>
+                      </head>
+                      <body>
+                        <h1>Мое предложение: {planet_name}</h1>
+                        <div class="alert alert-dark" role="alert">
+                          Эта планета близка к земле;
+                        </div>
+                        <div class="alert alert-success" role="alert">
+                            На ней есть вода и атмосфера;
+                        </div>
+                        <div class="alert alert-danger" role="alert">
+                            На ней есть небольшое магнитное поле;
+                        </div>
+                        <div class="alert alert-warning" role="alert">
+                            Наконец, она просто красива!
+                        </div>
+                      </body>
+                    </html>"""
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
