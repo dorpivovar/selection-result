25f91e1a6eb6867bbdc529b86d78a16764241a21
dorpivovar <armandooganesyan@gmail.com>
2024-02-09 10:59:19 +0300

    first commit

--- program.py
+++ program.py
@@ -1,9 +1,45 @@
-def main():
-    print('My first git program')
-    print('And I change it every day')
-    print('Again')
-    print('UFO came and added this line')
+from flask import Flask, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def default():
+    return 'Миссия Колонизация Марса'
+
+
+@app.route('/index')
+def index():
+    return "И на Марсе будут яблони цвести!"
+
+
+@app.route('/promotion')
+def promotion():
+    return """<html>
+                <body>
+                <p>Человечество вырастает из детства.</p>
+    <p>Человечеству мала одна планета.</p>
+    <p>Мы сделаем обитаемыми безжизненные пока планеты.</p>
+    <p>И начнем с Марса!</p>
+    <p>Присоединяйся!</p>
+    </body>
+    </html>"""
+
+
+@app.route('/image_mars')
+def image_mars():
+    return f"""<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <title>Привет, Марс!</title>
+                  </head>
+                  <body>
+                    <h1>Жди нас, Марс!</h1>
+                    <img src="{url_for('static', filename='img/img.png')}">
+                  </body>
+                </html>"""
 
 
 if __name__ == '__main__':
-    main()
+    app.run(port=8080, host='127.0.0.1')
